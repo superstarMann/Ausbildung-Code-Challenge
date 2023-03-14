f336bbc65c58df571e48b33a20c1bd72b0feedc4
LeeSeungHun <58622039+superstarMann@users.noreply.github.com>
2023-03-14 22:51:55 +0100

    file

--- src/components/library.js
+++ src/components/library.js
@@ -34,6 +34,10 @@ export const handleUpload = (e, setCsvObject) => {
 }
 
 
+
+
+// Der folgende Code wurde mit Hilfe eines Freundes erstellt.
+
 export const mySplit = (line, delimiter, ignore) => {
     let spt = [];
     let tmp = "";
@@ -67,7 +71,7 @@ export const mySplit = (line, delimiter, ignore) => {
     return spt;
   }
   
-
+// Der folgende Code wurde mit Hilfe eines Freundes erstellt.
 export const parsingCsv = (file, setCsvObject) => {
   let height, width;
   let obj = {
